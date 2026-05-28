66ed792a93d2ab29da9428b0fda1f73f692996a7
Harshit Chouhan <harshiitchouhan12@gmail.com>
2026-05-28 16:24:34 +0530

    prepare backend for vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -151,4 +151,11 @@ app.get("/", (req, res) => res.send("Swiggy proxy backend running ✅"));
 
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+  );
+}
+
+module.exports = app;
