44bfca122e49be30431d62c727d65bf652e2163d
Harshit Chouhan <harshiitchouhan12@gmail.com>
2026-05-28 20:43:45 +0530

    improve restaurant UI and responsiveness

--- src/Components/RestCard.jsx
+++ src/Components/RestCard.jsx
@@ -5,7 +5,8 @@ return(
 
      <Link to={"/city/delhi/" + swiggydata?.info?.id}>
      <div className="w-full transform transition duration-250 hover:scale-90">     <div className="relative">
-     <img className="w-full h-45 object-cover rounded-2xl  " src={"https://media-assets.swiggy.com/swiggy/image/upload/" + swiggydata?.info?.cloudinaryImageId} alt="RestImage" />
+     <img className="w-full h-32 sm:h-40 md:h-45 object-cover rounded-2xl" 
+     src={"https://media-assets.swiggy.com/swiggy/image/upload/" + swiggydata?.info?.cloudinaryImageId} alt="RestImage" />
 
           <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/40 to-transparent rounded-2xl">
           <p className="absolute bottom-2 left-4 text-white font-bold">
@@ -16,15 +17,15 @@ return(
      
      </div>
      <div >
-          <p className="text-black font-bold text-base pl-1 pt-2 truncate">{swiggydata?.info?.name}</p>
-          <p className="text-black font-semibold text-base pl-1 flex items-center">
-               <span className="text-green-700 pr-1 text-xl pb-1">★</span>
+     <p className="text-black font-bold text-sm sm:text-base pl-1 pt-2 truncate">{swiggydata?.info?.name}</p>
+          <p className="text-black font-semibold text-sm sm:text-base pl-1 flex items-center gap-1 whitespace-nowrap overflow-hidden">
+               <span className="text-green-700 text-base">★</span>
                {swiggydata?.info?.avgRatingString} • {swiggydata?.info?.sla?.slaString}
           </p>
-          <p className="text-gray-600 text-base pl-1 truncate">
+          <p className="text-gray-600 text-xs sm:text-base pl-1 truncate">
                {swiggydata?.info?.cuisines?.join(", ")}
-          </p>   
-          <p className="text-gray-600 text-base pl-1 truncate">{swiggydata?.info?.areaName}</p>
+     </p>   
+          <p className="text-gray-600 text-xs sm:text-base pl-1 truncate">{swiggydata?.info?.areaName}</p>
      </div>
      
           
