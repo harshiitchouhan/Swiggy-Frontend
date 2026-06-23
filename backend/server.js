2f9bd6a4b57e20302eac1db005499c0996bf6998
Harshit Chouhan <harshiitchouhan12@gmail.com>
2026-06-24 00:12:26 +0530

    Cors few err

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173", "https://swiggy-eat.vercel.app/"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://swiggy-eat.vercel.app"], credentials: true }));
 app.use(express.json());
 
 const LAT = "28.7069867";
